9d25bfaae112fa45af2868864c2712410286115e
g0vin-d <krishhhello555@gmail.com>
2022-01-18 18:33:53 +0530

    Updated settings

--- CashMyCar/settings.py
+++ CashMyCar/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g$&u6$lt^3&0ot2--(dhfe3b!e)x2l)v$zie@=w!5023k)j09%'
+SECRET_KEY = os.environ.get('SECRET_KEY');
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cashmycar.herokuapp.com']
 
 
 # Application definition
